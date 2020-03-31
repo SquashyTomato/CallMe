1a1b318a036ed90c1a2e177fd64920e5de711319
SquashyTomato <lgcheesecakeyt@gmail.com>
2020-03-31 02:27:11 +0100

    Added Message Length Limit

--- Events/Client/message.js
+++ Events/Client/message.js
@@ -29,6 +29,8 @@ module.exports = (client, config, msg) => {
 
                         let recChannel = client.channels.cache.get(sendTo);
 
+                        if (msg.content.length === 1900) return msg.react('❌');
+
                         if (msg.author.id === config.permissions.owner) return recChannel.send('<' + config.misc.emote + '> | <' + config.misc.devEmote + '> **' + msg.author.username + '**#' + msg.author.discriminator + ': ' + msg.content);
                         else return recChannel.send('<' + config.misc.emote + '> | **' + msg.author.username + '**#' + msg.author.discriminator + ': ' + msg.content);
                     }
